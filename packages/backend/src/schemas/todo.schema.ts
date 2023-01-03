9805c3be393528abe0adfe1c7d468695bdd84171
ruslankotliar <ruslankot101@knu.ua>
2023-01-03 13:55:55 +0100

    ref: Changed schema validation

--- packages/backend/src/schemas/todo.schema.ts
+++ packages/backend/src/schemas/todo.schema.ts
@@ -4,5 +4,6 @@ import Joi from 'joi';
 export const taskSchema = Joi.object().keys({
   title: Joi.string().max(50).required(),
   description: Joi.string().allow('').trim(),
-  private: Joi.boolean()
+  private: Joi.boolean(),
+  completed: Joi.boolean()
 });
