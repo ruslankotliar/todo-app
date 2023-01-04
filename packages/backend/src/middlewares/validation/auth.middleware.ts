6c9970566b4a68fb4c772659201e988fbb61ae09
ruslankotliar <ruslankot101@knu.ua>
2023-01-04 22:25:30 +0100

    ref: Slightly changed auth middleware

--- packages/backend/src/middlewares/validation/auth.middleware.ts
+++ packages/backend/src/middlewares/validation/auth.middleware.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
+import { CustomRequest } from '../../types/middleware.type';
 import { verifyJwt } from '../../utils/jwt';
 
 export async function verifyToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) throw new Error('You are not logged in.');
 
   const decoded = await verifyJwt(token);
   if (!decoded) throw new Error('Invalid JWT');
+  (req as CustomRequest).token = decoded;
+
   next();
 }
