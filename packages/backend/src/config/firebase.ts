cdac518fef2188774a9ab468efb6b86e457a8304
ruslankotliar <ruslankot101@knu.ua>
2023-01-21 22:14:13 +0100

    Update firebase config

--- packages/backend/src/config/firebase.ts
+++ packages/backend/src/config/firebase.ts
@@ -1,23 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-// import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: 'todo-app-e5bb4.firebaseapp.com',
-  projectId: 'todo-app-e5bb4',
-  storageBucket: 'todo-app-e5bb4.appspot.com',
-  messagingSenderId: '876689789964',
-  appId: '1:876689789964:web:90df1196863a38d32eca29',
-  measurementId: 'G-G0VFN66530'
+  authDomain: 'todo-app-5f8ad.firebaseapp.com',
+  projectId: 'todo-app-5f8ad',
+  storageBucket: 'todo-app-5f8ad.appspot.com',
+  messagingSenderId: '979544141238',
+  appId: '1:979544141238:web:698357f4ba4d3a7ae3a517'
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-// export const analytics = getAnalytics(app);
